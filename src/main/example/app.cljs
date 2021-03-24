c00f1d4d882c83758409a22490d5df3f6ac29efb
Peter Strömberg <pez@pezius.com>
2021-03-24 07:12:06 +0100

    Create button component

--- src/main/example/app.cljs
+++ src/main/example/app.cljs
@@ -7,48 +7,52 @@
    [example.events]
    [example.subs]))
 
-;; must use defonce and must refresh full app so metro can fill these in
-;; at live-reload time `require` does not exist and will cause errors
-;; must use path relative to :output-dir
-
 (defonce splash-img (js/require "../assets/shadow-cljs.png"))
 
-(def styles
-  {:container   {:flex             1
-                 :background-color :white
-                 :align-items      :center
-                 :justify-content  :flex-start
-                 :padding-top      50}
-   :counter     {:font-weight   :bold
-                 :font-size     24
-                 :color         :blue
-                 :margin-bottom 20}
-   :button      {:font-weight      :bold
-                 :font-size        18
-                 :padding          6
-                 :background-color :blue
-                 :border-radius    999
-                 :margin-bottom    20}
-   :button-text {:padding-left  12
-                 :padding-right 12
-                 :font-weight   :bold
-                 :font-size     18
-                 :color         :white}
-   :logo        {:width  200
-                 :height 200}
-   :creds       {:font-weight :normal
-                 :font-size   15
-                 :color       :blue}})
+(defn- button [{:keys [style text-style on-press
+                       disabled? disabled-style disabled-text-style]
+                :or {on-press #()}} text]
+  [:> rn/TouchableOpacity {:style (cond-> {:font-weight      :bold
+                                           :font-size        18
+                                           :padding          6
+                                           :background-color :blue
+                                           :border-radius    999
+                                           :margin-bottom    20}
+                                    :always (merge style)
+                                    disabled? (merge {:background-color "#aaaaaa"}
+                                                     disabled-style))
+                           :on-press on-press
+                           :disabled disabled?}
+   [:> rn/Text {:style (cond-> {:padding-left  12
+                                :padding-right 12
+                                :font-weight   :bold
+                                :font-size     18
+                                :color         :white}
+                         :always (merge text-style)
+                         disabled? (merge {:color :white}
+                                          disabled-text-style))} 
+    text]])
 
 (defn root []
-  [:> rn/View {:style (:container styles)}
-   [:> rn/Text {:style (:counter styles)} "Clicked: " @(rf/subscribe [:get-counter])]
-   [:> rn/TouchableOpacity {:style    (:button styles)
-                            :on-press #(rf/dispatch [:inc-counter])}
-    [:> rn/Text {:style (:button-text styles)} "Click me, I'll count"]]
-   [:> rn/Image {:style  (:logo styles)
+  [:> rn/View {:style {:flex             1
+                       :background-color :white
+                       :align-items      :center
+                       :justify-content  :flex-start
+                       :padding-top      50}}
+   [:> rn/Text {:style {:font-weight   :bold
+                        :font-size     24
+                        :color         :blue
+                        :margin-bottom 20}} "Clicked: " @(rf/subscribe [:get-counter])]
+   [button {:on-press #(rf/dispatch [:inc-counter])
+            :disabled? true
+            :style {:background-color :blue}}
+    "Click me, I'll count"]
+   [:> rn/Image {:style {:width  200
+                         :height 200}
                  :source splash-img}]
-   [:> rn/Text {:style (:creds styles)} "Using: shadow-cljs+expo+reagent+re-frame"]])
+   [:> rn/Text {:style {:font-weight :normal
+                        :font-size   15
+                        :color       :blue}} "Using: shadow-cljs+expo+reagent+re-frame"]])
 
 (defn start
   {:dev/after-load true}
