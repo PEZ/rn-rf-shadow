a6c40c2944974b6e5f56b89e9f4e6cabcf0f9d00
Peter Strömberg <pez@pezius.com>
2021-04-30 11:14:50 +0200

    Start at zero

--- src/main/example/db.cljs
+++ src/main/example/db.cljs
@@ -2,9 +2,9 @@
   (:require [re-frame.core :as rf]))
 
 ;; initial state of app-db
-(defonce app-db {:counter 0
+(def app-db {:counter 0
                  :counter-tappable? true
-                 :last-fibs [1 1]})
+                 :last-fibs [0 1]})
 
 (comment
   @re-frame.db/app-db)
