9f5347d4305835a13f5d9c20bbafec167489e7fe
Peter Strömberg <pez@pezius.com>
2023-06-03 17:33:06 +0200

    Install and use StackNavigator

--- src/main/example/app.cljs
+++ src/main/example/app.cljs
@@ -7,44 +7,52 @@
             [re-frame.core :as rf]
             ["react-native" :as rn]
             [reagent.core :as r]
-            ["@react-navigation/native" :as rnn]))
+            ["@react-navigation/native" :as rnn]
+            ["@react-navigation/native-stack" :as rnn-stack]))
 
 (def shadow-splash (js/require "../assets/shadow-cljs.png"))
 (def cljs-splash (js/require "../assets/cljs.png"))
 
+(defonce Stack (rnn-stack/createNativeStackNavigator))
+
+(defn home []
+  (r/with-let [counter (rf/subscribe [:get-counter])
+               tap-enabled? (rf/subscribe [:counter-tappable?])]
+    [:> rn/View {:style {:flex 1
+                         :padding-vertical 50
+                         :justify-content :space-between
+                         :align-items :center
+                         :background-color :white}}
+     [:> rn/View {:style {:align-items :center}}
+      [:> rn/Text {:style {:font-weight   :bold
+                           :font-size     72
+                           :color         :blue
+                           :margin-bottom 20}} @counter]
+      [button {:on-press #(rf/dispatch [:inc-counter])
+               :disabled? (not @tap-enabled?)
+               :style {:background-color :blue}}
+       "Tap me, I'll count"]]
+     [:> rn/View
+      [:> rn/View {:style {:flex-direction :row
+                           :align-items :center
+                           :margin-bottom 20}}
+       [:> rn/Image {:style {:width  160
+                             :height 160}
+                     :source cljs-splash}]
+       [:> rn/Image {:style {:width  160
+                             :height 160}
+                     :source shadow-splash}]]
+      [:> rn/Text {:style {:font-weight :normal
+                           :font-size   15
+                           :color       :blue}}
+       "Using: shadow-cljs+expo+reagent+re-frame"]]
+     [:> StatusBar {:style "auto"}]]))
+
 (defn root []
-  (let [counter @(rf/subscribe [:get-counter])
-        tap-enabled? @(rf/subscribe [:counter-tappable?])]
-    [:> rnn/NavigationContainer
-     [:> rn/View {:style {:flex 1
-                          :padding-vertical 50
-                          :justify-content :space-between
-                          :align-items :center
-                          :background-color :white}}
-      [:> rn/View {:style {:align-items :center}}
-       [:> rn/Text {:style {:font-weight   :bold
-                            :font-size     72
-                            :color         :blue
-                            :margin-bottom 20}} counter]
-       [button {:on-press #(rf/dispatch [:inc-counter])
-                :disabled? (not tap-enabled?)
-                :style {:background-color :blue}}
-        "Tap me, I'll count"]]
-      [:> rn/View
-       [:> rn/View {:style {:flex-direction :row
-                            :align-items :center
-                            :margin-bottom 20}}
-        [:> rn/Image {:style {:width  160
-                              :height 160}
-                      :source cljs-splash}]
-        [:> rn/Image {:style {:width  160
-                              :height 160}
-                      :source shadow-splash}]]
-       [:> rn/Text {:style {:font-weight :normal
-                            :font-size   15
-                            :color       :blue}}
-        "Using: shadow-cljs+expo+reagent+re-frame"]]
-      [:> StatusBar {:style "auto"}]]]))
+  [:> rnn/NavigationContainer 
+   [:> Stack.Navigator
+    [:> Stack.Screen {:name "Home"
+                      :component #(r/as-element [home])}]]])
 
 (defn start
   {:dev/after-load true}
