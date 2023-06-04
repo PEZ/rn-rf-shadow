f35ee4a050eca883cc890069535d24d9eb0d5700
Peter Strömberg <pez@pezius.com>
2023-06-04 21:58:04 +0200

    Use react-native-web ^0.18.12

--- src/main/example/widgets.cljs
+++ src/main/example/widgets.cljs
@@ -5,17 +5,17 @@
 (defn button [{:keys [style text-style on-press
                        disabled? disabled-style disabled-text-style]
                 :or {on-press #()}} text]
-  [:> rn/TouchableOpacity {:style (cond-> {:font-weight      :bold
-                                           :font-size        18
-                                           :padding          6
-                                           :background-color :blue
-                                           :border-radius    999
-                                           :margin-bottom    20}
-                                    :always (merge style)
-                                    disabled? (merge {:background-color "#aaaaaa"}
-                                                     disabled-style))
-                           :on-press on-press
-                           :disabled disabled?}
+  [:> rn/Pressable {:style (cond-> {:font-weight      :bold
+                                    :font-size        18
+                                    :padding          6
+                                    :background-color :blue
+                                    :border-radius    999
+                                    :margin-bottom    20}
+                             :always (merge style)
+                             disabled? (merge {:background-color "#aaaaaa"}
+                                              disabled-style))
+                    :on-press on-press
+                    :disabled disabled?}
    [:> rn/Text {:style (cond-> {:padding-left  12
                                 :padding-right 12
                                 :font-weight   :bold
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const createExpoWebpackConfigAsync = require('@expo/webpack-config');
-
-module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
-  config.devServer.watchOptions.ignored = [/.*/]; 
-  return config;
-};
